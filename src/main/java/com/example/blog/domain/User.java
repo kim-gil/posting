18b2cbca855d20a61d3c5a7adcdc220530e8c87c
RohSeongHoon <scubarsh@naver.com>
2022-05-03 23:00:18 +0900

    querydsl,h2 database를 테스트하기 위한 엔티티 구현

---
+++ src/main/java/com/example/blog/domain/User.java
@@ -0,0 +1,27 @@
+package com.example.blog.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue
+    private long id;
+    @Column(columnDefinition = "VARCHAR(16)")
+    private String userId;
+    @Column(columnDefinition = "VARCHAR(16)")
+    private String username;
+
+    public User(String userId, String username) {
+        this.userId = userId;
+        this.username = username;
+    }
+}
