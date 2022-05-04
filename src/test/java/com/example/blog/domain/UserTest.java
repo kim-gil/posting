067f491e184edd3dcd4f3c4ab3e8900f9f4a3d40
RohSeongHoon <scubarsh@naver.com>
2022-05-04 17:18:41 +0900

    테스트 코드생성

---
+++ src/test/java/com/example/blog/domain/UserTest.java
@@ -0,0 +1,35 @@
+package com.example.blog.domain;
+
+import com.example.blog.repository.UserRepository;
+import com.example.blog.repository.UserRepositorySupport;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
+class UserTest {
+    @Autowired
+    UserRepository userJpaRepository;
+
+    @Autowired
+    UserRepositorySupport userRepositorySupport;
+
+    @Test
+    public void queryDslTest() {
+        userJpaRepository.deleteAll();
+        User user = new User("testId", "testName");
+        userJpaRepository.save(user);
+
+        List<User> result = userRepositorySupport.findByUserId(user.getUserId());
+
+        System.out.println(result.size());
+        System.out.println(result.get(0).getUserId());
+
+        User user1 = new User("testId1", "testName1");
+        userJpaRepository.save(user1);
+        List<User> result1 = userJpaRepository.findByUserId(user1.getUserId());
+        System.out.println(result1.get(0).getUserId());
+    }
+}
