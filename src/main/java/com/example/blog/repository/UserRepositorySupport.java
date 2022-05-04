8fade67abb63a141a49d2c529aa9f2cbfef8600c
RohSeongHoon <scubarsh@naver.com>
2022-05-04 17:18:28 +0900

    querydsl 테스트 코드 생성

---
+++ src/main/java/com/example/blog/repository/UserRepositorySupport.java
@@ -0,0 +1,27 @@
+package com.example.blog.repository;
+import static com.example.blog.domain.QUser.user;
+import com.example.blog.domain.User;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class UserRepositorySupport extends QuerydslRepositorySupport {
+    private final JPAQueryFactory queryFactory;
+
+    public UserRepositorySupport(JPAQueryFactory queryFactory) {
+        super(User.class);
+        this.queryFactory = queryFactory;
+    }
+
+    public List<User> findByUserId(String userId){
+        return queryFactory
+                .selectFrom(user)
+                .where(user.userId.eq(userId))
+                .fetch();
+    }
+
+
+}
