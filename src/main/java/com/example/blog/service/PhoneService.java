fd5efe7a5112fc2a5180a52d556d0335d9e59d48
RohSeongHoon <scubarsh@naver.com>
2022-05-07 16:29:37 +0900

    firebasestore에 데이터를 조회하고 업데이트하는 메소드 추가

--- src/main/java/com/example/blog/service/PhoneService.java
+++ src/main/java/com/example/blog/service/PhoneService.java
@@ -2,6 +2,8 @@ package com.example.blog.service;
 
 import com.example.blog.domain.Phone;
 import com.google.api.core.ApiFuture;
+import com.google.cloud.firestore.DocumentReference;
+import com.google.cloud.firestore.DocumentSnapshot;
 import com.google.cloud.firestore.Firestore;
 import com.google.cloud.firestore.WriteResult;
 import com.google.firebase.cloud.FirestoreClient;
@@ -14,6 +16,28 @@ public class PhoneService {
     public static final String COL_NAME = "phone";
 
     public String savePhoneInfo(Phone phone) throws ExecutionException, InterruptedException {
+        Firestore dbFireStore = FirestoreClient.getFirestore();
+        ApiFuture<WriteResult> collectionsApiFuture =
+                dbFireStore.collection(COL_NAME).document(phone.getPhoneName()).set(phone);
+        return collectionsApiFuture.get().getUpdateTime().toString();
+    }
+
+    public Phone getPhoneInfo(String phoneNm) throws ExecutionException, InterruptedException {
+        Firestore dbFireStore = FirestoreClient.getFirestore();
+        DocumentReference documentReference =
+                dbFireStore.collection(COL_NAME).document(phoneNm);
+        ApiFuture<DocumentSnapshot> future = documentReference.get();
+        DocumentSnapshot document = future.get();
+
+        Phone phone = null;
+        if (!document.exists()) {
+            return null;
+        }
+        phone = document.toObject(Phone.class);
+        return phone;
+    }
+
+    public String updatePhoneInfo(Phone phone) throws ExecutionException, InterruptedException {
         Firestore dbFireStore = FirestoreClient.getFirestore();
         ApiFuture<WriteResult> collectionsApiFuture =
                 dbFireStore.collection(COL_NAME).document(phone.getPhoneName()).set(phone);
