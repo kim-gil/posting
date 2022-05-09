ea48ffb548d11b8601f421ac13fa52d4613c5d55
RohSeongHoon <scubarsh@naver.com>
2022-05-09 23:48:45 +0900

    firebaseMessaging 을 빈등록하는 설정 생성

--- src/main/java/com/example/blog/config/FirebaseConfig.java
+++ src/main/java/com/example/blog/config/FirebaseConfig.java
@@ -3,7 +3,10 @@ package com.example.blog.config;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import com.google.firebase.messaging.FirebaseMessaging;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 
 import javax.annotation.PostConstruct;
 import java.io.FileInputStream;
@@ -11,19 +14,18 @@ import java.io.IOException;
 
 @Configuration
 public class FirebaseConfig {
-    @PostConstruct
-    public void init() {
-        try {
-            FileInputStream serviceAccountFile = new FileInputStream("src/main/resources/serviceAccount-File.json");
-            FirebaseOptions options = FirebaseOptions
-                    .builder()
-                    .setCredentials(GoogleCredentials.fromStream(serviceAccountFile))
-                    .setDatabaseUrl("https://example-ac805.firebaseio.com")
-                    .build();
+    @Bean
+    public FirebaseApp init() throws IOException {
+        FileInputStream serviceAccountFile = new FileInputStream("src/main/resources/serviceAccount-File.json");
+        FirebaseOptions options = FirebaseOptions
+                .builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccountFile))
+                .build();
+        return FirebaseApp.initializeApp(options);
+    }
 
-            FirebaseApp.initializeApp(options);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    @Bean
+    public FirebaseMessaging firebaseMessaging(FirebaseApp app) throws IOException {
+        return FirebaseMessaging.getInstance(app);
     }
 }
