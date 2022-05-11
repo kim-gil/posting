11d47093aeab54634015bdf1973f1d1c31484241
RohSeongHoon <scubarsh@naver.com>
2022-05-11 17:01:04 +0900

    firebase  SDK적용  firebaseMessaging객체를 주입받아 메시지 전송 makeMessage() 메소드로 메시지 생성

--- src/main/java/com/example/blog/service/FirebaseMessagingService.java
+++ src/main/java/com/example/blog/service/FirebaseMessagingService.java
@@ -1,65 +1,36 @@
 package com.example.blog.service;
 
-import com.example.blog.request.FcmMessage;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.FirebaseMessagingException;
 import com.google.firebase.messaging.Message;
 import com.google.firebase.messaging.Notification;
-import okhttp3.MediaType;
-import okhttp3.OkHttpClient;
-import okhttp3.RequestBody;
 import org.springframework.stereotype.Service;
-
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.util.List;
 
 @Service
 public class FirebaseMessagingService {
-    private static final String API_URL = "https://fcm.googleapis.com/v1/projects/example-ac805/messages:send";
-    private final ObjectMapper objectMapper;
+   private FirebaseMessaging firebaseMessaging;
 
-    public FirebaseMessagingService(ObjectMapper objectMapper) {
-        this.objectMapper = objectMapper;
+    public FirebaseMessagingService(FirebaseMessaging firebaseMessaging) {
+        this.firebaseMessaging = firebaseMessaging;
     }
 
-
-    public String getAccessToken() throws IOException {
-        FileInputStream serviceAccountFile = new FileInputStream("src/main/resources/serviceAccount-File.json");
-        GoogleCredentials googleCredentials = GoogleCredentials
-                .fromStream(serviceAccountFile)
-                .createScoped(List.of("https://www.googleapis.com/auth/cloud-platform"));
-        googleCredentials.refreshIfExpired();
-        return googleCredentials.getAccessToken().getTokenValue();
+    public String sendMessage(String targetToken, String title, String body) throws FirebaseMessagingException {
+        Message message = makeMessage(targetToken, title, body);
+       return firebaseMessaging.send(message);
     }
 
-    public void sendMessage(String token, String title, String content) throws JsonProcessingException {
-        String message = makeMessage(token, title, content);
-
-        OkHttpClient httpClient = new OkHttpClient();
-        RequestBody requestBody = RequestBody.create(message, MediaType.get);
-
-
-    }
-
-    private String makeMessage(String token, String title, String content) throws JsonProcessingException {
-         FcmMessage fcmMessage = FcmMessage
+    public Message makeMessage(String targetToken, String title, String body) {
+        Notification notification = Notification
                 .builder()
-                .validateOnly(true)
-                .message(Message
-                        .builder()
-                        .setToken(token)
-                        .setNotification(Notification
-                                .builder()
-                                .setTitle(title)
-                                .setBody(content)
-                                .setImage(null)
-                                .build())
-                        .build())
+                .setTitle(title)
+                .setBody(body)
                 .build();
-         return objectMapper.writeValueAsString(fcmMessage);
+        Message message = Message
+                .builder()
+                .setNotification(notification)
+                .setToken(targetToken)
+                .build();
+        return message;
     }
-
 
 }
