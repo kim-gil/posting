0ee66a4d71ed521a6efae4d5245ed556f16f83c3
RohSeongHoon <scubarsh@naver.com>
2022-05-11 17:01:36 +0900

    FirebaseMessaging 빈등록 설정 추가

--- src/main/java/com/example/blog/config/FirebaseConfig.java
+++ src/main/java/com/example/blog/config/FirebaseConfig.java
@@ -6,21 +6,24 @@ import com.google.firebase.FirebaseOptions;
 import com.google.firebase.messaging.FirebaseMessaging;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
 
-import javax.annotation.PostConstruct;
+
 import java.io.FileInputStream;
 import java.io.IOException;
 
 @Configuration
 public class FirebaseConfig {
     @Bean
-    public FirebaseApp init() throws IOException {
+    public FirebaseApp firebaseApp() throws IOException {
         FileInputStream serviceAccountFile = new FileInputStream("src/main/resources/serviceAccount-File.json");
         FirebaseOptions options = FirebaseOptions
                 .builder()
                 .setCredentials(GoogleCredentials.fromStream(serviceAccountFile))
                 .build();
         return FirebaseApp.initializeApp(options);
+    }
+    @Bean
+    public FirebaseMessaging firebaseMessaging(FirebaseApp firebaseApp){
+        return FirebaseMessaging.getInstance(firebaseApp);
     }
 }
