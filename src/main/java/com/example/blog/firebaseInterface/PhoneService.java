a659d87dab58ea7c080b0c02bffb73745ed121dc
RohSeongHoon <scubarsh@naver.com>
2022-05-12 17:29:35 +0900

    패키지 이동

---
+++ src/main/java/com/example/blog/firebaseInterface/PhoneService.java
@@ -0,0 +1,52 @@
+package com.example.blog.firebaseInterface;
+
+import com.example.blog.domain.Phone;
+import com.google.api.core.ApiFuture;
+import com.google.cloud.firestore.DocumentReference;
+import com.google.cloud.firestore.DocumentSnapshot;
+import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.WriteResult;
+import com.google.firebase.cloud.FirestoreClient;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.ExecutionException;
+
+@Service
+public class PhoneService {
+    public static final String COL_NAME = "phone";
+
+    public String savePhoneInfo(Phone phone) throws ExecutionException, InterruptedException {
+        Firestore dbFireStore = FirestoreClient.getFirestore();
+        ApiFuture<WriteResult> collectionsApiFuture =
+                dbFireStore.collection(COL_NAME).document(phone.getPhoneName()).set(phone);
+        return collectionsApiFuture.get().getUpdateTime().toString();
+    }
+
+    public Phone getPhoneInfo(String phoneNm) throws ExecutionException, InterruptedException {
+        Firestore dbFireStore = FirestoreClient.getFirestore();
+        DocumentReference documentReference =
+                dbFireStore.collection(COL_NAME).document(phoneNm);
+        ApiFuture<DocumentSnapshot> future = documentReference.get();
+        DocumentSnapshot document = future.get();
+
+        Phone phone = null;
+        if (!document.exists()) {
+            return null;
+        }
+        phone = document.toObject(Phone.class);
+        return phone;
+    }
+
+    public String updatePhoneInfo(Phone phone) throws ExecutionException, InterruptedException {
+        Firestore dbFireStore = FirestoreClient.getFirestore();
+        ApiFuture<WriteResult> collectionsApiFuture =
+                dbFireStore.collection(COL_NAME).document(phone.getPhoneName()).set(phone);
+        return collectionsApiFuture.get().getUpdateTime().toString();
+    }
+    public String deletePhoneInfo(String phoneName){
+        Firestore dbFireStore = FirestoreClient.getFirestore();
+        ApiFuture<WriteResult> writeResult =
+                dbFireStore.collection(COL_NAME).document(phoneName).delete();
+        return "삭제 완료";
+    }
+}
