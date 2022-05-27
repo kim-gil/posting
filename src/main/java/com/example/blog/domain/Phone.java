f7be76ec6abe5f6b9f81c38bfbc539e739f14caa
RohSeongHoon <scubarsh@naver.com>
2022-05-27 14:56:36 +0900

    jpa로 db에 데이터를 저장하기위해 table설정

--- src/main/java/com/example/blog/domain/Phone.java
+++ src/main/java/com/example/blog/domain/Phone.java
@@ -1,17 +1,29 @@
 package com.example.blog.domain;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.*;
+
 @Getter
 @Setter
 @NoArgsConstructor
+@Entity
+@Table(name = "phone")
 public class Phone {
+    @GeneratedValue
+    @Id
+    private int id;
+    @Column(columnDefinition = "VARCHAR(32)", nullable = false)
     private String phoneName;
+    @Column(columnDefinition = "VARCHAR(32)", nullable = false)
     private String company;
+    @Column(columnDefinition = "VARCHAR(32)")
     private String telecom;
 
+    @Builder
     public Phone(String phoneName, String company, String telecom) {
         this.phoneName = phoneName;
         this.company = company;
